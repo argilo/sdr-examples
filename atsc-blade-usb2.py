8dd8897928610fff807ca487ad599f68949d74a2
Clayton Smith <argilo@gmail.com>
2014-02-09 13:55:30 -0500

    Update copyright dates.

--- atsc-blade-usb2.py
+++ atsc-blade-usb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env /usr/bin/python
 
-# Copyright 2013 Clayton Smith (argilo@gmail.com)
+# Copyright 2013-2014 Clayton Smith (argilo@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
