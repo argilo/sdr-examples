eff7c128dd0f45cbed03c100b61fd278ea8a8980
Clayton Smith <argilo@gmail.com>
2022-07-29 21:46:19 -0400

    Add basic EIA-608 support

--- ntsc/ntsc-encode.py
+++ ntsc/ntsc-encode.py
@@ -60,6 +60,35 @@ def addNonVisibleLine():
     ntsc_signal += [BLANKING_LEVEL] * 654
 
 
+def addLine21(char_one, char_two):
+    global ntsc_signal
+
+    bits = []
+    for char in (char_one, char_two):
+        parity = 1
+        for bit_num in range(7):
+            bit = (char >> bit_num) & 1
+            parity ^= bit
+            bits.append(bit)
+        bits.append(parity)
+
+    ntsc_signal += SYNCH_PULSE
+    addBackPorch()
+    ntsc_signal += [BLANKING_LEVEL] * 7
+
+    for x in range(7 * 24):
+        ntsc_signal.append(25.0 + 25.0 * math.sin(2 * math.pi * (x-5.5) / 24))
+
+    ntsc_signal += [0.0] * 18  # S1
+    ntsc_signal += [0.0] * 24  # S2
+    ntsc_signal += [50.0] * 24  # S3
+
+    for bit in bits:
+        ntsc_signal += [50.0 if bit else 0.0] * 24
+
+    ntsc_signal += [BLANKING_LEVEL] * 29
+
+
 def addFirstHalfFrame():
     global ntsc_signal
     ntsc_signal += SYNCH_PULSE
@@ -90,31 +119,47 @@ def addPixel(p):
     ntsc_signal += [BLACK_LEVEL + (WHITE_LEVEL - BLACK_LEVEL) * Em]
 
 
+line21_codes = [
+    (0x14, 0x25),
+    (0x14, 0x25),
+    (0x14, 0x2D),
+    (0x14, 0x2D),
+    (0x14, 0x70),
+    (0x14, 0x70),
+]
+cc_text = "Test One Two Three Four Five"
+for x in range(0, len(cc_text), 2):
+    line21_codes.append((ord(cc_text[x]), ord(cc_text[x+1])))
+
 ntsc_signal = []
 
-# Generate even field
-ntsc_signal += INTERVALS
-for x in range(13):
-    addNonVisibleLine()
-for line in range(0, 480, 2):
-    ntsc_signal += SYNCH_PULSE
-    addBackPorch()
-    for x in range(line * 640 + 2, (line+1) * 640 - 2):
-        addPixel(pixels[x])
-    ntsc_signal += FRONT_PORCH
-addFirstHalfFrame()
+for line21_code in line21_codes:
+    # Generate even field
+    ntsc_signal += INTERVALS
+    for x in range(13):
+        if x == 11:
+            addLine21(line21_code[0], line21_code[1])
+        else:
+            addNonVisibleLine()
+    for line in range(0, 480, 2):
+        ntsc_signal += SYNCH_PULSE
+        addBackPorch()
+        for x in range(line * 640 + 2, (line+1) * 640 - 2):
+            addPixel(pixels[x])
+        ntsc_signal += FRONT_PORCH
+    addFirstHalfFrame()
 
-# Generate odd field
-ntsc_signal += INTERVALS + EXTRA_HALF_LINE
-for x in range(12):
-    addNonVisibleLine()
-addSecondHalfFrame()
-for line in range(1, 481, 2):
-    ntsc_signal += SYNCH_PULSE
-    addBackPorch()
-    for x in range(line * 640 + 2, (line+1) * 640 - 2):
-        addPixel(pixels[x])
-    ntsc_signal += FRONT_PORCH
+    # Generate odd field
+    ntsc_signal += INTERVALS + EXTRA_HALF_LINE
+    for x in range(12):
+        addNonVisibleLine()
+    addSecondHalfFrame()
+    for line in range(1, 481, 2):
+        ntsc_signal += SYNCH_PULSE
+        addBackPorch()
+        for x in range(line * 640 + 2, (line+1) * 640 - 2):
+            addPixel(pixels[x])
+        ntsc_signal += FRONT_PORCH
 
 ntsc_signal = [0.75 - (0.25/40) * x for x in ntsc_signal]
 
